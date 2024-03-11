1d3fdfadc22f2221eabe5ee7269e6b51a6034c35
”ukrWily” <ukrwily@gmail.com>
2024-03-11 19:46:56 +0200

    Added tabs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -8,9 +8,34 @@ const tabs = (headerSElector, tabSelector, contentSelector, activeClass) => {
     content.forEach((item) => {
       item.style.display = "none";
     });
+
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
   }
 
-  function showTabContent() {}
+  function showTabContent(i = 0) {
+    content[i].style.display = "block";
+    tab[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener("click", (e) => {
+    const target = e.target;
+    if (
+      (target && target.classList.contains(tabSelector.replace(/\./, ""))) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ""))
+    ) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
 };
 
 export default tabs;
